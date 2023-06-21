e5cbf2bb2eef34ef5b7800694f69c683b1ac7dde
piratheesh611 <137283754+piratheesh611@users.noreply.github.com>
2023-06-21 05:06:23 -0700

    Add files via upload

---
+++ myfirstprogram - 04.py
@@ -0,0 +1,16 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/msi/AppData/Local/Programs/Python/Python311/Piratheesh grade - 09.py
+Hello,World!
+ What iS your name? Piratheesh
+Hello  Piratheesh
+>>> age=12
+>>> print(age)
+12
+>>> Weight=44
+>>> print(Weight)
+44
+>>> Height=98
+>>> print(Weight)
+44
